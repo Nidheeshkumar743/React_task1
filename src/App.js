8f10df137e3dcd51e5e3475730635469c667032b
Nidheeshkumar743 <144110412+Nidheeshkumar743@users.noreply.github.com>
2023-10-20 14:29:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import React from 'react';
+
+import Text from './components/Text';
+
+
+function App() {
+
+  return (
+    <>
+      <div className="container my-3">
+        <Text heading="Responsive paragraph word counter"/>
+      </div> 
+    </>
+
+  );
+}
+
+export default App;
